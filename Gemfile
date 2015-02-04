9ee6cda63c97a7e7b44f1f3ca5de58f0f57c9ecd
Rob Zyskowski <zyskowski.rob@gmail.com>
2015-02-03 23:51:41 -0800

    views and assets

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'sinatra-assetpack'
+gem 'thin'
+gem 'therubyracer'
+gem 'less'
--- hoyoswelch.rb
+++ hoyoswelch.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require 'sinatra/assetpack'
 
 class HoyosWelch < Sinatra::Base
+    set :root, File.dirname(__FILE__)
+    register Sinatra::AssetPack
+
+    assets do
+        css_dir = 'less'
+        serve '/css', from: css_dir
+
+        css :base, '/css/base.css', [
+            '/css/base.css'
+        ]
+    end
+
+
     get '/' do
         erb :base
     end
