93840b50145332754c7e4333fafbf73bd4cdeecb
Rob Zyskowski <zyskowski.rob@gmail.com>
2015-02-10 11:48:30 -0800

    add less agaain

--- hoyoswelch.rb
+++ hoyoswelch.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
+require 'sinatra/assetpack'
+require 'less'
 
 class HoyosWelch < Sinatra::Base
     set :root, File.dirname(__FILE__)
+
+    register Sinatra::AssetPack
+
+    assets {
+        serve '/css',   from: 'public/css'
+
+        css :application, '/css/base.less', [
+            '/css/base.css'
+        ]
+
+        css_compression :less
+    }
+
 
     get '/' do
         erb :base
