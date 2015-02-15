349fb9966559fc805e696cf4577f3c2c8b8e6540
Rob Zyskowski <zyskowski.rob@gmail.com>
2015-02-15 01:18:48 -0800

    new modles`

--- app.rb
+++ app.rb
@@ -16,7 +16,21 @@ class Guest
     include DataMapper::Resource
     property :id,         Serial
     property :name,       String
+    property :num_guests, Integer
+    property :attending,  Boolean
+    property :advice,     Text
+    property :songs,      Text
     property :created_at, DateTime
+
+    has n, :plus_ones
+end
+
+class PlusOne
+    include DataMapper::Resource
+    property :id,   Serial
+    property :name, String
+
+    belongs_to :guest
 end
 DataMapper.auto_migrate!
 
@@ -26,14 +40,20 @@ class App < Sinatra::Base
     register Sinatra::AssetPack
 
     assets {
-        serve '/css',   from: 'media/less'
-        serve '/images', from: 'media/images'
+        serve '/css',       from: 'media/less'
+        serve '/images',    from: 'media/images'
+        serve '/js',        from: 'media/js'
 
-        css :application, [
+        css :app, [
             '/css/base.css'
         ]
 
+        js :app, [
+            '/js/app.js'
+        ]
+
         css_compression :less
+        js_compression  :jsmin
     }
 
 
@@ -45,15 +65,24 @@ class App < Sinatra::Base
         erb :guests, :locals => {:guests => Guest.all}
     end
 
-    post '/thank-you' do
-        name = params['name']
-
+    post '/submit' do
         @guest = Guest.create(
-            :name => "#{name}",
+            :name => params['name'],
+            :attending => params['attending'],
+            :num_guests => params['num_guests'],
+            :advice => params['advice'],
+            :songs => params['songs'],
             :created_at => Time.now
         )
 
-        erb :thank_you, :locals => {:name => name}
+        params['plus_ones'].each do |name|
+            PlusOne.create(
+                :name => name,
+                :guest => @guest
+            )
+        end
+
+        erb :thank_you, :locals => {:name => params['name']}
     end
 
     run! if __FILE__ == $0
---
+++ media/js/app.js
@@ -0,0 +1,44 @@
+$(function () {
+    var $nav = $('.nav');
+    var $welcome = $('.section.welcome');
+
+    // Sticky navbar
+    $(window).scroll(function (event) {
+        if ($(this).scrollTop() > $welcome.height() && $nav.css('position') !== 'fixed') {
+            $nav.css({
+                position: 'fixed',
+                top: '0px'
+            });
+        } else if ($(this).scrollTop() < $welcome.height() && $nav.css('position') === 'fixed') {
+            $nav.css({
+                position: 'static'
+            });
+        }
+    });
+
+    // Scroll to section
+    $('.nav_elem').click(function (event) {
+        var id = $(event.target).attr('href');
+        $('html, body').animate({
+            scrollTop: ($(id).offset().top - (2 * $nav.height()))
+        }, 300);
+    });
+
+
+    // Plus one input
+    $('#num_guests').change(function (event) {
+        var $guest_names_container = $('.guest_names_container');
+        var val = $(event.target).val();
+
+        $guest_names_container.empty();
+        for (var i = 0; i < val; i++) {
+            var prototype = '' +
+                '<div class="block">' +
+                    '<label for="plus_one_name_' + i +'" class="plus_one_name">Guest name</label>' +
+                    '<input type="text" id="plus_one_name' + i +'" name="plus_ones[]" />' +
+                '</div>'
+            $guest_names_container.append(prototype);
+        }
+    });
+
+});
